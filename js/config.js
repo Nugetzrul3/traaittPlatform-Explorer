4511465e0c54ca7f6d11abae8f94f4720517f86e
traaittPlatform <traaittPlatform@github.com>
2019-12-29 13:35:16 +0100

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const ExplorerConfig = {
+  apiBaseUrl: 'https://blockapi.traaittplatform.com',
+  ticker: 'ETRX',
+  maxSupply: 74400000000000,
+  emissionSpeed: 23,
+  decimalPoints: 2,
+  blockTargetTime: 30,
+  startYear: 2019,
+  genesisTimestamp: 1574888787,
+  emissionCurveInterval: 20000,
+  emissionCurveDataPoints: 15000,
+  addressPrefix: 158,
+  forkHeights: []
+}
